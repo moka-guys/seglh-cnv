6cc0a681b80c3e1481d982e5e0ce036dbcccfc64
David Brawand <david.brawand@gmail.com>
2021-05-12 20:48:59 +0100

    automatic pon creation mode

--- exomedepth/readCount.R
+++ exomedepth/readCount.R
@@ -53,7 +53,7 @@ pcn<-vector()
 for (bamfile in args[4:length(args)]) {
     if (endsWith(bamfile,'.bam')) {
         bam<-append(bam,bamfile)
-        message(paste('          Track:',bamfile))
+        message(paste('     Alignments:',bamfile))
     } else if (endsWith(bamfile,'.RData')) {
         pcn<-append(pcn,bamfile)
         message(paste('         Counts:',bamfile))
@@ -116,74 +116,84 @@ testsamplenames<-refsamplenames
 normals<-which(substr(refsamplenames,1,nchar(normalprefix)) == normalprefix)
 tests<-which(substr(testsamplenames,1,nchar(normalprefix)) != normalprefix)
 if (length(normals)>0) {
-    message("Will use Panel of Normals...")
+    message(paste("-> Panel of Normals [",length(normals),"]..."))
     refsamplenames<-refsamplenames[normals]
     testsamplenames<-testsamplenames[tests]
+    # ensure there are 0 (pon), 2 or more (batch) samples
+    if (length(testsamplenames)==1) {
+        message('NOTE: Single test sample. Batch correlation will be 1.')
+        testsamplenames<-rep(testsamplenames,2)
+    }
 } else {
-    message('Will use intra-batch normalisation...')
+    message('-> Batch normalisation')
 }
 
 #
-# Calculate RPKM
+# Process test samples if any
 #
-calcRPKM<-function(c,l) c/(l*sum(c)/10^6)
-counts.len<-counts$end-counts$start+1
-rpkm<-apply(counts[,testsamplenames],2,function(x) calcRPKM(x,counts.len))
+if (length(testsamplenames)==0) {
+    message('Creating Panel of Normals (PoN)...')
+    # save normal counts
+    save(list=c(
+                "refsamplenames",  # supplied normals
+                "counts"),         # normal counts
+         file = args[1])
+} else {
+    message('Building reference sets...')
+    # Calculate RPKM
+    calcRPKM<-function(c,l) c/(l*sum(c)/10^6)
+    counts.len<-counts$end-counts$start+1
 
-#
-# Calculate batch statistics (all samples excluding normals if any)
-#
-batch.cv<-apply(rpkm,2,function(r) sd(r)/mean(r)*100)
-batch.cor<-cor(rpkm)
-diag(batch.cor)<-NA
+    rpkm<-apply(counts[,testsamplenames],2,function(x) calcRPKM(x,counts.len))
 
-#
-# Pick reference sample set
-#
-selectReferenceSet<-function(testsample) {
-  refsamples<-refsamplenames[which(refsamplenames!=testsample)]
-  select.reference.set(
-    test.counts=counts[,testsample],
-    reference.counts=as.matrix(counts[,refsamples]),
-    bin.length=(counts$end - counts$start)
-  )
+    # Calculate batch statistics (all samples excluding normals if any)
+    batch.cv<-apply(rpkm,2,function(r) sd(r)/mean(r)*100)
+    batch.cor<-cor(rpkm)
+    diag(batch.cor)<-NA
+
+    # Pick reference sample set
+    selectReferenceSet<-function(testsample) {
+      refsamples<-refsamplenames[which(refsamplenames!=testsample)]
+      suppressWarnings(select.reference.set(
+        test.counts=counts[,testsample],
+        reference.counts=as.matrix(counts[,refsamples]),
+        bin.length=(counts$end - counts$start)
+      ))
+    }
+    refsets<-lapply(testsamplenames, selectReferenceSet)
+    names(refsets)<-testsamplenames
+
+    # statistics output
+    stats<-data.frame()
+    for (testsample in testsamplenames) {
+        d<-cbind(
+                 sample=testsample,
+                 refsamples=which(refsets[[testsample]]$summary.stats$selected),
+                 refsets[[testsample]]$summary.stats[which(refsets[[testsample]]$summary.stats$selected),
+                                        c('correlations','expected.BF','phi','RatioSd','mean.p','median.depth')],
+                 batch.maxcor=max(batch.cor[testsample,],na.rm=TRUE),
+                 batch.mediancor=median(batch.cor[testsample,],na.rm=TRUE),
+                 coeff.var=batch.cv[testsample]
+                )
+        rownames(d)<-testsample
+        stats<-rbind(stats,d)
+    }
+    write.table(stats, file=sub("[.][^.]*$", ".csv", args[1], perl=TRUE), sep='\t', quote=FALSE, row.names=FALSE)
+
+    # save read count table as Rdata
+    save(list=c(
+                "referenceFasta",  # reference fasta
+                "refsamplenames",  # selected reference samples (normals if provided)
+                "testsamplenames", # supplied test samples
+                "counts",          # all read counts
+                "rois",            # regions of interest (full)
+                "batch.cv",        # Coefficient of Variation for each sample in batch
+                "batch.cor",       # Correlation within batch
+                "refsets",         # picked reference sets
+                "rpkm",            # RPKM calculation
+                "calcRPKM",        # function to calculate RPKM
+                "stats"),          # model and betch statistics
+         file = args[1])
+
 }
-refsets<-lapply(testsamplenames, selectReferenceSet)
-names(refsets)<-testsamplenames
-
-#
-# statistics output
-#
-stats<-data.frame()
-for (testsample in testsamplenames) {
-    d<-cbind(
-             sample=testsample,
-             refsamples=which(refsets[[testsample]]$summary.stats$selected),
-             refsets[[testsample]]$summary.stats[which(refsets[[testsample]]$summary.stats$selected),
-                                    c('correlations','expected.BF','phi','RatioSd','mean.p','median.depth')],
-             batch.maxcor=max(batch.cor[testsample,],na.rm=TRUE),
-             batch.mediancor=median(batch.cor[testsample,],na.rm=TRUE),
-             coeff.var=batch.cv[testsample]
-            )
-    rownames(d)<-testsample
-    stats<-rbind(stats,d)
-}
-write.table(stats, file=sub("[.][^.]*$", ".csv", args[1], perl=TRUE), sep='\t', quote=FALSE, row.names=FALSE)
-
-#
-# save read count table as Rdata
-# 
-
-save(list=c(
-            "referenceFasta",  # reference fasta
-            "refsamplenames",  # selected reference samples (normals if provided)
-            "counts",          # all read counts
-            "rois",            # regions of interest (full)
-            "batch.cv",        # Coefficient of Variation for each sample in batch
-            "batch.cor",       # Correlation within batch
-            "refsets",         # picked reference sets
-            "rpkm",            # RPKM calculation
-            "calcRPKM",        # function to calculate RPKM
-            "stats"),          # model and betch statistics
-     file = args[1])
-
+message('DONE')
