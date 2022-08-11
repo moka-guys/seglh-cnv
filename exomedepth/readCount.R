755c2f173d3fa3b812f9de86345050cf0a28ff23
David Brawand <dbrawand@gmail.com>
2022-08-11 12:40:25 +0100

    readCount picks reference fo rbathc and pon simultaneously

--- exomedepth/readCount.R
+++ exomedepth/readCount.R
@@ -122,26 +122,24 @@ counts<-counts.computed
 #
 # select normal samples (if 3+ specified)
 #
-refsamplenames<-colnames(counts)[which(endsWith(colnames(counts),'.bam'))]
-testsamplenames<-refsamplenames
-# NORMAL selection
-normals<-which(substr(refsamplenames,1,nchar(normalprefix)) == normalprefix)
-tests<-which(substr(testsamplenames,1,nchar(normalprefix)) != normalprefix)
-if (length(normals)>0) {
+bams<-colnames(counts)[which(endsWith(colnames(counts),'.bam'))]
+normals<-which(substr(bams,1,nchar(normalprefix)) == normalprefix)
+tests<-which(substr(bams,1,nchar(normalprefix)) != normalprefix)
+# test samples are same regardless of normalisation method
+testsamplenames<-bams[tests]
+# refsamplenames
+refsamplenames<-list()
+if (length(tests)>=3) {
+	message('-> Batch normalisation')
+	refsamplenames[['batch']]<-bams[tests]
+}
+if (length(normals)>=2) {
     message(paste("-> Panel of Normals [",length(normals),"]..."))
-    refsamplenames<-refsamplenames[normals]
-    testsamplenames<-testsamplenames[tests]
-    # ensure there are 0 (pon), 2 or more (batch) samples
-    if (length(testsamplenames)==1) {
-        message('NOTE: Single test sample. Batch correlation will be 1.')
-        testsamplenames<-rep(testsamplenames,2)
-    }
-} else {
-    message('-> Batch normalisation')
+	refsamplenames[['pon']]<-bams[normals]
 }
 
 #
-# Process test samples if any
+# Create PoN if no test samples
 #
 if (length(testsamplenames)==0) {
     message('Creating Panel of Normals (PoN)...')
@@ -150,115 +148,136 @@ if (length(testsamplenames)==0) {
 	if (length(bad.normals)>0) {
 		message("Removing normals that have insufficient ROI coverage")
 		print(bad.normals)
-		counts<-counts[,-which(colnames(counts) %in% bad.normals)],
+		counts<-counts[,-which(colnames(counts) %in% bad.normals)]
 	}
     # save normal counts
     save(list=c("refsamplenames",  # supplied normals
                 "counts"),         # normal counts
          file = args[1])
-
-} else {
-    message('Building reference sets...')
-    # Calculate RPKM
-    calcRPKM<-function(c,l) c/(l*sum(c)/10^6)
-    counts.len<-counts$end-counts$start+1
-    rpkm<-apply(counts[,testsamplenames],2,function(x) calcRPKM(x,counts.len))
-
-    # Calculate batch statistics (all samples excluding normals if any)
-    batch.cv<-apply(rpkm,2,function(r) sd(r)/mean(r)*100)
-    batch.cor<-cor(rpkm)
-    diag(batch.cor)<-NA
-
-    # Pick reference sample set
-    selectReferenceSet<-function(testsample) {
-        message(paste('Picking reference for',testsample))
-        # exclude test sample
-        refsamples<-refsamplenames[which(refsamplenames!=testsample)]
-        # if PoN && XY && Sex -> sex match
-        hasPon<-length(normals)>0
-        hasXY<-any(rois[,1]=="X") || any(rois[,1]=="Y")
-        tssx<-str_extract(testsample,sex)
-        rcsx<-str_extract(refsamples,sex)
-        hasSex<-!(is.na(tssx) || any(is.na(rcsx)))
-        if (hasPon && hasXY && hasSex) {
-            message(paste('Enforcing sex match of', testsample, 'to reference'))
-            refsamples<-refsamples[which(rcsx==tssx)]
-        } else if (!hasPon && hasXY && !is.na(tssx)) {
-            # if no PoN, XY targets and testsample has sex, do sex match if at least 2 matched references exist within batch
-            refsamples_candidates<-refsamples[which(rcsx==tssx)]
-            if (length(refsamples_candidates)>2) {
-                message(paste('Enforcing sex match of', testsample, 'within batch'))
-                refsamples<-refsamples_candidates
-            }
-        }
-        # pick reference  
-        suppressWarnings(selected<-select.reference.set(
-            test.counts=counts[,testsample],
-            reference.counts=as.matrix(counts[,refsamples]),
-            bin.length=(counts$end - counts$start)
-        ))
-        # if not enough reference samples, enlarge reference set
-        # -> accept lower power in favour of specificity
-        ref.count<-which(selected$summary.stats$selected)
-        if (ref.count < min.refs) {
-            # create new df
-            new.summary.stats <- selected$summary.stats
-            new.summary.stats$selected <- FALSE
-            # get row with highest expected.BF after min.refs rows
-            BF.ranks <- order(new.summary.stats$expected.BF, decreasing=T, na.last=T)
-            best.BF.rank <- which.min(BF.ranks[min.refs:length(BF.ranks)])+min.refs-1
-            # mark selected row 
-            new.summary.stats$selected[best.BF.rank] <- TRUE
-            # build new selected reference
-            new.selected <- list(
-                reference.choice = as.character(new.summary.stats$ref.samples[1:best.BF.rank]),
-                summary.stats = new.summary.stats,
-                min.refs = min.refs)
-            message(paste('Enlarged reference set:',ref.count,'=>', best.BF.rank))
-            selected<-new.selected
-        } else {
-            message(paste('Reference set size:',ref.count))
-            selected$min.refs<-1
-        }
-        selected
-    }
-    refsets<-lapply(testsamplenames, selectReferenceSet)
-    names(refsets)<-testsamplenames
-
-    # statistics output
-    stats<-data.frame()
-    for (testsample in testsamplenames) {
-        message(paste('Collecting statistics for',testsample))
-        d<-cbind(
-                 sample=testsample,
-                 min.refs=refsets[[testsample]]$min.refs,
-                 refsamples=which(refsets[[testsample]]$summary.stats$selected),
-                 refsets[[testsample]]$summary.stats[which(refsets[[testsample]]$summary.stats$selected),
-                                        c('correlations','expected.BF','phi','RatioSd','mean.p','median.depth')],
-                 batch.maxcor=max(batch.cor[testsample,],na.rm=TRUE),
-                 batch.mediancor=median(batch.cor[testsample,],na.rm=TRUE),
-                 coeff.var=batch.cv[testsample]
-                )
-        rownames(d)<-testsample
-        stats<-rbind(stats,d)
-    }
-    print(stats)
-    write.table(stats, file=sub("[.][^.]*$", ".csv", args[1], perl=TRUE), sep='\t', quote=FALSE, row.names=FALSE)
-
-    # save read count table as Rdata
-    save(list=c(
-                "referenceFasta",  # reference fasta
-                "refsamplenames",  # selected reference samples (normals if provided)
-                "testsamplenames", # supplied test samples
-                "counts",          # all read counts
-                "rois",            # regions of interest (full)
-                "batch.cv",        # Coefficient of Variation for each sample in batch
-                "batch.cor",       # Correlation within batch
-                "refsets",         # picked reference sets
-                "rpkm",            # RPKM calculation
-                "calcRPKM",        # function to calculate RPKM
-                "stats"),          # model and betch statistics
-         file = args[1])
-
+	quit(status=0, save='no')
 }
+
+#
+# find reference sets
+#
+message('Building reference sets...')
+# Pick reference sample set from batch and pon
+selectReferenceSet<-function(testsample) {
+	selected.refs<-list()
+	hasXY<-any(rois[,1]=="X") || any(rois[,1]=="Y")
+	for (rs in names(refsamplenames)) {
+		message(paste('Picking', toupper(rs), 'reference for', testsample))
+
+		# exclude test sample
+		refsamples<-refsamplenames[[rs]][which(refsamplenames[[rs]]!=testsample)]
+		
+		# if PoN && XY && Sex -> sex match
+		hasPon<-rs=='pon'
+		tssx<-str_extract(testsample, sex)
+		rcsx<-str_extract(refsamples, sex)
+		hasSex<-!(is.na(tssx) || any(is.na(rcsx)))
+		if (hasPon && hasXY && hasSex) {
+			message(paste('Enforcing sex match of', testsample, 'to reference'))
+			refsamples<-refsamples[which(rcsx==tssx)]
+		} else if (!hasPon && hasXY && !is.na(tssx)) {
+			# if no PoN, XY targets and testsample has sex, do sex match if at least 2 matched references exist within batch
+			refsamples_candidates<-refsamples[which(rcsx==tssx)]
+			if (length(refsamples_candidates)>2) {
+				message(paste('Enforcing sex match of', testsample, 'within batch'))
+				refsamples<-refsamples_candidates
+			}
+		}
+
+		# pick reference  
+		suppressWarnings(selected<-select.reference.set(
+			test.counts=counts[,testsample],
+			reference.counts=as.matrix(counts[,refsamples]),
+			bin.length=(counts$end - counts$start)
+		))
+
+		# if not enough reference samples, enlarge reference set
+		ref.count<-which(selected$summary.stats$selected)
+		if (ref.count < min.refs) {
+			# create new df
+			new.summary.stats <- selected$summary.stats
+			new.summary.stats$selected <- FALSE
+			# get row with highest expected.BF after min.refs rows
+			BF.ranks <- order(new.summary.stats$expected.BF, decreasing=T, na.last=T)
+			best.BF.rank <- which.min(BF.ranks[min.refs:length(BF.ranks)])+min.refs-1
+			# mark selected row 
+			new.summary.stats$selected[best.BF.rank] <- TRUE
+			# build new selected reference
+			new.selected <- list(
+				reference.choice = as.character(new.summary.stats$ref.samples[1:best.BF.rank]),
+				summary.stats = new.summary.stats,
+				min.refs = min.refs)
+			message(paste('Enlarged reference set:',ref.count,'=>', best.BF.rank))
+			selected<-new.selected
+		} else {
+			message(paste('Reference set size:',ref.count))
+			selected$min.refs<-1
+		}
+
+		# save reference set to list
+		selected.refs[[rs]]<-selected
+	}
+	selected.refs
+}
+refsets<-lapply(testsamplenames, selectReferenceSet)
+names(refsets)<-testsamplenames
+
+#
+# statistics output
+#
+
+# Calculate RPKM
+calcRPKM<-function(c,l) c/(l*sum(c)/10^6)
+counts.len<-counts$end-counts$start+1
+rpkm<-apply(counts[,testsamplenames],2,function(x) calcRPKM(x,counts.len))
+
+# Calculate batch statistics (all samples excluding normals if any)
+batch.cv<-apply(rpkm,2,function(r) sd(r)/mean(r)*100)
+batch.cor<-cor(rpkm)
+diag(batch.cor)<-NA
+stats<-data.frame()
+
+save.image()
+
+for (rs in names(refsamplenames)) {
+	for (testsample in testsamplenames) {
+		message(paste('Collecting stats for',testsample,'with',toupper(rs),'reference set'))
+		d<-cbind(
+				 refset=rs,
+				 sample=testsample,
+				 min.refs=refsets[[testsample]][[rs]]$min.refs,
+				 refsamples=which(refsets[[testsample]][[rs]]$summary.stats$selected),
+				 refsets[[testsample]][[rs]]$summary.stats[which(refsets[[testsample]][[rs]]$summary.stats$selected),
+										c('correlations','expected.BF','phi','RatioSd','mean.p','median.depth')],
+				 batch.maxcor=max(batch.cor[testsample,],na.rm=TRUE),
+				 batch.mediancor=median(batch.cor[testsample,],na.rm=TRUE),
+				 coeff.var=batch.cv[testsample]
+				)
+		rownames(d)<-testsample
+		stats<-rbind(stats,d)
+	}
+}
+print(stats)
+write.table(stats, file=sub("[.][^.]*$", ".csv", args[1], perl=TRUE), sep='\t', quote=FALSE, row.names=FALSE)
+
+#
+# save read count table as Rdata
+#
+save(list=c(
+			"referenceFasta",  # reference fasta
+			"refsamplenames",  # selected reference samples (normals if provided)
+			"testsamplenames", # supplied test samples
+			"counts",          # all read counts
+			"rois",            # regions of interest (full)
+			"batch.cv",        # Coefficient of Variation for each sample in batch
+			"batch.cor",       # Correlation within batch
+			"refsets",         # picked reference sets
+			"rpkm",            # RPKM calculation
+			"calcRPKM",        # function to calculate RPKM
+			"stats"),          # model and betch statistics
+	 file = args[1])
 message('DONE')
